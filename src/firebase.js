0e5f3d5b22238c50aaa810619ad09ad7c0c403d9
Jaspreet Bhagat <jaspreet.bhagat17@gmail.com>
2024-06-26 19:25:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "./firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDY8AddQg5Sd8Cdyd7qQttkiOpud20o7lg",
-    authDomain: "linkedin-clone-d06cb.firebaseapp.com",
-    projectId: "linkedin-clone-d06cb",
-    storageBucket: "linkedin-clone-d06cb.appspot.com",
-    messagingSenderId: "34581783075",
-    appId: "1:34581783075:web:996c9c96c453e746d3c796",
-    measurementId: "G-TRP3C917N0"
+    //your own firebaseconfigs
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
